790efae39a4811a9f10bf6d382580e8e4d50be66
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2016-12-31 00:50:46 +0200

    Add reading data from mq135 and hds10

--- MamGuard.ino
+++ MamGuard.ino
@@ -1,5 +1,9 @@
 #include <OLED_I2C.h>
 
+const uint8_t HDS10_PIN = A2;
+const uint8_t MQ135_PIN = A3;
+const uint8_t SCREEN_SDA_PIN = A4;
+const uint8_t SCREEN_SCL_PIN = A5;
 
 const uint8_t SCREEN_WIDTH = 128;
 const uint8_t SCREEN_HALF_WIDTH = SCREEN_WIDTH / 2;
@@ -11,17 +15,17 @@ const uint8_t SCREEN_YELOW_HEIGHT = 16;
 const uint8_t SCREEN_BLUE_HEIGHT = SCREEN_HEIGHT - SCREEN_YELOW_HEIGHT;
 const uint8_t SCREEN_BLUE_HALF_HEIGHT = SCREEN_BLUE_HEIGHT / 2;
 
-const uint8_t SCREEN_SDA = A4;
-const uint8_t SCREEN_SCL = A5;
-
 const uint8_t MARGINE = 4;
 const uint8_t FIRST_LINE = 16;
 const uint8_t SECOND_LINE = 40;
 
 
+OLED  myOLED(SCREEN_SDA_PIN, SCREEN_SCL_PIN);
 
-OLED  myOLED(SCREEN_SDA, SCREEN_SCL);
+
 bool is_warning = false;
+int mq135_sensorValue = 0;
+int hds10_sensorValue = 0;
 
 extern uint8_t MediumNumbers[];
 extern uint8_t SmallFont[];
@@ -45,6 +49,12 @@ void draw_warning()
   myOLED.drawRoundRect(0,0, SCREEN_WIDTH -1, SCREEN_YELOW_HEIGHT - 1);
 }
 
+void read_data_from_sensor()
+{
+  mq135_sensorValue = analogRead(MQ135_PIN);
+  hds10_sensorValue = analogRead(HDS10_PIN);
+}
+
 void setup()
 {
   myOLED.begin();
@@ -52,6 +62,9 @@ void setup()
 
 void loop()
 {
+  read_data_from_sensor();
+
+  
   myOLED.clrScr();
   myOLED.setFont(SmallFont);
   if (!is_warning)
@@ -65,11 +78,10 @@ void loop()
 
   myOLED.setFont(MediumNumbers);
 
-  //#TODO: Add constants on lines.
-  myOLED.printNumI(random(0, 101), MARGINE, FIRST_LINE + MARGINE,3,'0');
-  myOLED.printNumI(random(0, 101), SCREEN_HALF_WIDTH + MARGINE, FIRST_LINE + MARGINE,3,'0');
-  myOLED.printNumI(random(0, 101), MARGINE, SECOND_LINE + MARGINE,3,'0');
-  myOLED.printNumI(random(0, 101), SCREEN_HALF_WIDTH + MARGINE, SECOND_LINE + MARGINE,3,'0');
+  myOLED.printNumI(mq135_sensorValue, MARGINE, FIRST_LINE + MARGINE,3,'0');
+  myOLED.printNumI(0, SCREEN_HALF_WIDTH + MARGINE, FIRST_LINE + MARGINE,3,'0');
+  myOLED.printNumI(hds10_sensorValue, MARGINE, SECOND_LINE + MARGINE,3,'0');
+  myOLED.printNumI(0, SCREEN_HALF_WIDTH + MARGINE, SECOND_LINE + MARGINE,3,'0');
 
   draw_borders();
 
