d7d4f288d0f93d1a8275a74bdfe630311eef74f5
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2016-12-31 00:40:33 +0200

    Updated drawing borders and printing numbers

--- MamGuard.ino
+++ MamGuard.ino
@@ -15,6 +15,9 @@ const uint8_t SCREEN_SDA = A4;
 const uint8_t SCREEN_SCL = A5;
 
 const uint8_t MARGINE = 4;
+const uint8_t FIRST_LINE = 16;
+const uint8_t SECOND_LINE = 40;
+
 
 
 OLED  myOLED(SCREEN_SDA, SCREEN_SCL);
@@ -26,29 +29,20 @@ extern uint8_t SmallFont[];
 
 void draw_borders()
 {
-  // Vertical
-  myOLED.drawLine(0, SCREEN_YELOW_HEIGHT, SCREEN_WIDTH, SCREEN_YELOW_HEIGHT);
+  myOLED.drawRoundRect(0, SCREEN_YELOW_HEIGHT, SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1);
+  
+  // Vertical line in center
   myOLED.drawLine(0, SCREEN_YELOW_HEIGHT + SCREEN_BLUE_HALF_HEIGHT, SCREEN_WIDTH, SCREEN_YELOW_HEIGHT + SCREEN_BLUE_HALF_HEIGHT);
-  myOLED.drawLine(0, SCREEN_HEIGHT - 1, SCREEN_WIDTH, SCREEN_HEIGHT - 1);
 
-  // Horizontal
-  myOLED.drawLine(0, SCREEN_YELOW_HEIGHT, 0, SCREEN_HEIGHT);
+  // Horizontal line in center
   myOLED.drawLine(SCREEN_HALF_WIDTH, SCREEN_YELOW_HEIGHT, SCREEN_HALF_WIDTH, SCREEN_HEIGHT);
-  myOLED.drawLine(SCREEN_WIDTH - 1, SCREEN_YELOW_HEIGHT, SCREEN_WIDTH - 1, SCREEN_HEIGHT);
 }
 
 void draw_warning()
 {
-  myOLED.print("! < WARNNING > !", CENTER, MARGINE);
+  myOLED.print("!!! < WARNNING > !!!", CENTER, MARGINE);
 
   myOLED.drawRoundRect(0,0, SCREEN_WIDTH -1, SCREEN_YELOW_HEIGHT - 1);
-//  for (auto x = 0; x < SCREEN_WIDTH; ++x)
-//  {
-//    for (auto y = 0; y < SCREEN_YELOW_HEIGHT; ++y)
-//    {
-//      myOLED.invPixel(x, y);
-//    }
-//  }
 }
 
 void setup()
@@ -72,10 +66,10 @@ void loop()
   myOLED.setFont(MediumNumbers);
 
   //#TODO: Add constants on lines.
-  myOLED.print(String(random(101, 2000)), LEFT + MARGINE, 16 + MARGINE);
-  myOLED.print(String(random(101, 200)), RIGHT, 16 + MARGINE);
-  myOLED.print(String(random(101, 200)), LEFT + MARGINE, 40 + MARGINE);
-  myOLED.print(String(random(101, 200)), RIGHT, 40 + MARGINE);
+  myOLED.printNumI(random(0, 101), MARGINE, FIRST_LINE + MARGINE,3,'0');
+  myOLED.printNumI(random(0, 101), SCREEN_HALF_WIDTH + MARGINE, FIRST_LINE + MARGINE,3,'0');
+  myOLED.printNumI(random(0, 101), MARGINE, SECOND_LINE + MARGINE,3,'0');
+  myOLED.printNumI(random(0, 101), SCREEN_HALF_WIDTH + MARGINE, SECOND_LINE + MARGINE,3,'0');
 
   draw_borders();
 
