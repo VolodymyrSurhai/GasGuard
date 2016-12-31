5472f8d6d795d108fd4c06e356fb811741dc41d5
Volodymyr Surgai <volodymyr.surgai@gmail.com>
2016-12-31 12:36:00 +0200

    Add dat sensor reading

--- MamGuard.ino
+++ MamGuard.ino
@@ -1,9 +1,16 @@
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
 #include <OLED_I2C.h>
 
 const uint8_t HDS10_PIN = A2;
 const uint8_t MQ135_PIN = A3;
+
 const uint8_t SCREEN_SDA_PIN = A4;
 const uint8_t SCREEN_SCL_PIN = A5;
+
+const uint8_t DHT_PIN = 13;
+const auto DHT_TYPE = DHT11;
 
 const uint8_t SCREEN_WIDTH = 128;
 const uint8_t SCREEN_HALF_WIDTH = SCREEN_WIDTH / 2;
@@ -21,11 +28,13 @@ const uint8_t SECOND_LINE = 40;
 
 
 OLED  myOLED(SCREEN_SDA_PIN, SCREEN_SCL_PIN);
-
+DHT_Unified dht(DHT_PIN, DHT_TYPE);
 
 bool is_warning = false;
-int mq135_sensorValue = 0;
-int hds10_sensorValue = 0;
+uint32_t mq135_sensorValue = 0;
+uint32_t hds10_sensorValue = 0;
+uint32_t dht_temperature_sensorValue = 0;
+uint32_t dht_humidity_sensorValue = 0;
 
 extern uint8_t MediumNumbers[];
 extern uint8_t SmallFont[];
@@ -53,17 +62,25 @@ void read_data_from_sensor()
 {
   mq135_sensorValue = analogRead(MQ135_PIN);
   hds10_sensorValue = analogRead(HDS10_PIN);
+
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+
+  dht_temperature_sensorValue = event.temperature;
+
+  dht.humidity().getEvent(&event);
+  dht_humidity_sensorValue = event.relative_humidity;
 }
 
 void setup()
 {
   myOLED.begin();
+  dht.begin();
 }
 
 void loop()
 {
   read_data_from_sensor();
-
   
   myOLED.clrScr();
   myOLED.setFont(SmallFont);
@@ -79,9 +96,9 @@ void loop()
   myOLED.setFont(MediumNumbers);
 
   myOLED.printNumI(mq135_sensorValue, MARGINE, FIRST_LINE + MARGINE,3,'0');
-  myOLED.printNumI(0, SCREEN_HALF_WIDTH + MARGINE, FIRST_LINE + MARGINE,3,'0');
+  myOLED.printNumI(dht_temperature_sensorValue, SCREEN_HALF_WIDTH + MARGINE, FIRST_LINE + MARGINE,3,'0');
   myOLED.printNumI(hds10_sensorValue, MARGINE, SECOND_LINE + MARGINE,3,'0');
-  myOLED.printNumI(0, SCREEN_HALF_WIDTH + MARGINE, SECOND_LINE + MARGINE,3,'0');
+  myOLED.printNumI(dht_humidity_sensorValue, SCREEN_HALF_WIDTH + MARGINE, SECOND_LINE + MARGINE,3,'0');
 
   draw_borders();
 
